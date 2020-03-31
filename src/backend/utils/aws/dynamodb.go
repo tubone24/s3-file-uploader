a83e9309e36005260bd16abeded2d688f5e22e12
tubone24 <tubo.yyyuuu@gmail.com>
2020-04-01 00:01:25 +0900

    dynamodb

---
+++ src/backend/utils/aws/dynamodb.go
@@ -0,0 +1,56 @@
+package aws
+
+import (
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/labstack/gommon/log"
+)
+
+type DynamoDB struct {
+	client *dynamodb.DynamoDB
+}
+
+var dynamoSession = session.Must(session.NewSessionWithOptions(session.Options{
+	SharedConfigState: session.SharedConfigEnable,
+}))
+
+var dynamoDBInstance = &DynamoDB{client: dynamodb.New(dynamoSession)}
+
+func GetDynamoDBInstance() *DynamoDB {
+	return dynamoDBInstance
+}
+
+func (d *DynamoDB) Scan(tableName string) ([]map[string]*dynamodb.AttributeValue, error) {
+	params := &dynamodb.ScanInput{
+		TableName: &tableName,
+	}
+	res, err := d.client.Scan(params)
+	if err != nil {
+		return nil, err
+	}
+
+	data := res.Items
+	lastEvaluatedKey := res.LastEvaluatedKey
+	for lastEvaluatedKey != nil {
+		params := &dynamodb.ScanInput{
+			TableName: &tableName,
+			ExclusiveStartKey: lastEvaluatedKey,
+		}
+		res, err := d.client.Scan(params)
+		if err != nil {
+			log.Error(err)
+			return nil, err
+		}
+		data = append(data, res.Items...)
+		lastEvaluatedKey = res.LastEvaluatedKey
+	}
+
+
+	return data, nil
+}
+
+func AttributeValueToString(key string, value *dynamodb.AttributeValue) map[string]string {
+	stringItem := map[string]string {}
+	stringItem[key] = value.String()
+	return stringItem
+}
