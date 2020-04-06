62b8105a5491c09fe6ccd3da8f51ddea8472a839
tubone24 <tubo.yyyuuu@gmail.com>
2020-04-07 00:08:21 +0900

    testcase: util

---
+++ src/backend/utils/util_test.go
@@ -0,0 +1,35 @@
+package utils_test
+
+import (
+	"github.com/tubone24/s3-file-uploader/src/backend/utils"
+	"testing"
+)
+
+func TestGetContentType(t *testing.T) {
+	patterns := []struct {
+		key string
+		expected string
+	}{
+		{"test.txt", "text/plain"},
+		{"test.tsv", "text/tab-separated-values"},
+		{"test.csv", "text/csv"},
+		{"test.json", "application/json"},
+		{"test.pdf", "application/pdf"},
+		{"test.xls", "application/vnd.ms-excel"},
+		{"test.xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"},
+		{"test.ppt", "application/vnd.ms-powerpoint"},
+		{"test.pptx", "application/vnd.openxmlformats-officedocument.presentationml.presentation"},
+		{"test.doc", "application/msword"},
+		{"test.docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"},
+		{"test.zip", "application/zip"},
+		{"test.lzh", "application/x-lzh"},
+		//{"test.tar.gz", "application/x-tar"},
+	}
+
+	for idx, pattern := range patterns {
+		actual := utils.GetContentType(pattern.key)
+		if pattern.expected != actual {
+			t.Errorf("testcase%d: expected: %s, actual %s", idx, pattern.expected, actual)
+		}
+	}
+}
